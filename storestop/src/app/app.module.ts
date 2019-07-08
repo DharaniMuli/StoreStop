1eaeacd53c8cbc30e1c08fdec31ada464233ffae
DharaniMuli <46985863+DharaniMuli@users.noreply.github.com>
2019-07-07 20:03:20 -0500

    First push

---
+++ storestop/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AppstoreComponent } from './appstore/appstore.component';
+
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppstoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    Ng2SearchPipeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
